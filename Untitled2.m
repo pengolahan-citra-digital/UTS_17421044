434884ed3b1c7902590feb7e02cddae3b1806560
desysetiawati <valkaree21@gmail.com>
2019-04-23 14:24:03 +0700

    UTS

---
+++ Untitled2.m
@@ -0,0 +1,15 @@
+I = imread('D:/PENGOLAHAN CITRA DIGITAL sem 6/CITRA/eight.tif');
+canny = edge (I, 'canny');
+sobel = edge(I, 'sobel');
+
+subplot(2,2,1);
+imshow(I);
+title('Citra Asli');
+
+subplot(2,2,2);
+imshow(canny);
+title('Canny Operator ');
+
+subplot(2,2,3);
+imshow(sobel);
+title('Sobel Operator');
